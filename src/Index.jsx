009874bcf042d1daffb1084a8f8cb2436a0f3694
manishdahal10 <dahalmanish2001@gmail.com>
2025-01-22 10:57:58 +0545

    repo clone success(Manish)

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
+import React from "react";
 
-
-import "./index.css"
+import "./index.css";
 
 function Index() {
   return (
-    <>
-     Homepage
-    </>
-  )
+    <h1 className="text-4xl font-extrabold underline text-center text-white bg-gradient-to-r from-purple-500 to-grey-500 py-4 px-6 rounded-lg shadow-lg hover:shadow-2xl transform hover:scale-105 transition duration-300">
+      Ecom Tailwind Check
+    </h1>
+  );
 }
 
-export default Index
+export default Index;
