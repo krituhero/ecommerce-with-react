379fee0779cb6eb1ce2a93985a10d43066fec128
manishdahal10 <dahalmanish2001@gmail.com>
2025-01-22 12:47:08 +0545

    check

--- src/Index.jsx
+++ src/Index.jsx
@@ -5,7 +5,9 @@ import "./index.css";
 function Index() {
   return (
     <h1 className="text-4xl font-extrabold underline text-center text-white bg-gradient-to-r from-purple-500 to-grey-500 py-4 px-6 rounded-lg shadow-lg hover:shadow-2xl transform hover:scale-105 transition duration-300">
-      Ecom Tailwind Check done
+      Ecom Tailwind Check done Manish git fetch and git status git status
+      -uno(this is to check if your repo is uptodate with origin -uno shows not
+      list untracked files and only show updates for the current branch.)
     </h1>
   );
 }
