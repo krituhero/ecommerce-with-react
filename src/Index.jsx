61c7e5405c1dedf2d42a5050b29fc9bbce6bf0a6
krituhero <kritarthabaniya@gmail.com>
2025-01-23 13:22:30 +0545

    Completed the Navbar

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import Header from "./components/Header";
 
 import "./index.css";
 
 function Index() {
   return (
-    <h1 className="text-4xl font-extrabold underline text-center text-white bg-gradient-to-r from-purple-500 to-grey-500 py-4 px-6 rounded-lg shadow-lg hover:shadow-2xl transform hover:scale-105 transition duration-300">
-      Ecom Tailwind Check done
-    </h1>
+    <>
+    <Header />
+    
+    </>
   );
 }
 
