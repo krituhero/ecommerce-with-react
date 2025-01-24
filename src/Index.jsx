c1b1192b56a90ba6fbf95900984a0db5dc4dd0f6
krituhero <kritarthabaniya@gmail.com>
2025-01-24 18:54:06 +0545

    Changed structure

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 import "./index.css";
 
@@ -7,7 +8,7 @@ function Index() {
   return (
     <>
     <Header />
-    
+    <Footer/>
     </>
   );
 }
