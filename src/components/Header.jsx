0bb2a68e4c090b08c78be8286a5348a5568b8b66
manishdahal10 <dahalmanish2001@gmail.com>
2025-01-27 11:13:17 +0545

    contact double link page error solved

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -103,7 +103,7 @@ function Header() {
               </li>
               <li>
                 <Link
-                  to="Contact"
+                  to="/Contact"
                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 font-Roboto"
                 >
                   Contact
