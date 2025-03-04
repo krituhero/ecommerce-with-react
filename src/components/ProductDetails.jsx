6a6c583b6ba66d9fa73acae3f32021954c5164b0
krituhero <kritarthabaniya@gmail.com>
2025-03-04 11:58:28 +0545

    product details page

---
+++ src/components/ProductDetails.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+
+const ProductDetails = () => {
+  return (
+    <>
+    <Header />
+    <div className='mx-[100px]'>
+    
+    </div>
+    <Footer />
+    </>
+  )
+}
+
+export default ProductDetails
